b6adff4d837a182f7d4051bb6e807a0d555c8f0a
jae woo Jung <awfjol2008@gmail.com>
2025-12-27 20:22:26 +0900

    add ApplyService, Coupon, and CouponRepository with initial tests

---
+++ api/src/main/java/com/example/api/service/ApplyService.java
@@ -0,0 +1,24 @@
+package com.example.api.service;
+
+import com.example.api.domain.Coupon;
+import com.example.api.repository.CouponRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ApplyService {
+
+    private final CouponRepository couponRepository;
+
+    public void apply(Long userId) {
+        long count = couponRepository.count();
+
+        if (count > 100) {
+            return;
+        }
+
+        Coupon coupon = new Coupon(userId);
+        couponRepository.save(coupon);
+    }
+}
